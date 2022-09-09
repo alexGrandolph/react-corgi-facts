792535a64dd8d6d9a068be23d40e501c8bb5b85f
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-09 15:31:50 -0600

    feat: move tenative list of facts to be its own component

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
           Corgi Facts
         </p>
       </header>
-      <h3 className="corgi-facts">they are short</h3>
+      {/* <h3 className="corgi-facts">they are short</h3>
       <h3 className="corgi-facts">they are stubby</h3>
       <h3 className="corgi-facts">they have big ears</h3>
-      <h3 className="corgi-facts">they are good at sniffin</h3>
+      <h3 className="corgi-facts">they are good at sniffin</h3> */}
     </div>
   );
 }
---
+++ src/components/List.js
@@ -0,0 +1,13 @@
+function List() {
+  return (
+    <div className="list-container">
+      <h3 className="corgi-facts">they are short</h3>
+      <h3 className="corgi-facts">they are stubby</h3>
+      <h3 className="corgi-facts">they have big ears</h3>
+      <h3 className="corgi-facts">they are good at sniffin</h3>
+
+    </div>
+  )
+}
+
+export default List
