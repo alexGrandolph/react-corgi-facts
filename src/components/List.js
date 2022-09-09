4d5eeead9ea1f1d0915d547d65a35dfdc370da1a
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-09 16:26:49 -0600

    feat: final stylins

--- src/components/List.js
+++ src/components/List.js
@@ -2,10 +2,10 @@ function List() {
   return (
     <div className="list-container">
       <h1 className="list-header">Fun Facts About Corgis</h1>
-      <h3 className="corgi-facts">They are short</h3>
-      <h3 className="corgi-facts">They are stubby</h3>
-      <h3 className="corgi-facts">They have big ears</h3>
-      <h3 className="corgi-facts">They are good at sniffin</h3>
+      <h3 className="corgi-facts">- They are short</h3>
+      <h3 className="corgi-facts">- They are stubby</h3>
+      <h3 className="corgi-facts">- They have big ears</h3>
+      <h3 className="corgi-facts">- They are good at sniffin</h3>
     </div>
   )
 }
